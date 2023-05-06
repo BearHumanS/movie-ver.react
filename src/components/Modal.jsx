27ff1d9e75fdbe1859a30eae4da46decd2bcd837
BearHumanS <bye0651@naver.com>
2023-05-07 03:14:42 +0900

    modal 동작 기능 테스트 확인

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -2,18 +2,21 @@ import React from 'react'
 import './Modal.css'
 
 export default function Modal() {
-  const modal = document.querySelector('.modal')
   function openModal() {
+    const modal = document.querySelector('.modal')
     modal.classList.remove('hidden')
   }
   function closeModal() {
+    const modal = document.querySelector('.modal')
     modal.classList.add('hidden')
   }
   return (
     <>
       <button onClick={openModal}>테스트 버튼</button>
       <div className="modal hidden">
-        <div className="modal-overlay"></div>
+        <div
+          className="modal_overlay"
+          onClick={closeModal}></div>
         <div className="modal_content">
           <div className="movie-detail">테스트 중</div>
           <button
