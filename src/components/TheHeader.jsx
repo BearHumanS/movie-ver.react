471306871cb5683de336e055c3520946e0b61e09
BearHumanS <bye0651@naver.com>
2023-05-06 21:05:05 +0900

    Header component_actvie click event add

--- src/components/TheHeader.jsx
+++ src/components/TheHeader.jsx
@@ -1,8 +1,10 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './TheHeader.css'
 import { AiOutlineSearch } from 'react-icons/ai'
 
 export default function TheHeader() {
+  const [listActive, setListActive] = useState(false)
+  const [aboutActive, setAboutActive] = useState(false)
   function openBox() {
     const header = document.querySelector('header')
     header.classList.add('searching')
@@ -12,10 +14,26 @@ export default function TheHeader() {
       <header>
         <ul className="menu">
           <li>
-            <a href="#">영화목록</a>
+            <a
+              href="#"
+              className={`list ${listActive ? 'active' : ''}`}
+              onClick={() => {
+                setListActive(true)
+                setAboutActive(false)
+              }}>
+              영화목록
+            </a>
           </li>
           <li>
-            <a href="#">about me</a>
+            <a
+              href="#"
+              className={`about ${aboutActive ? 'active' : ''}`}
+              onClick={() => {
+                setAboutActive(true)
+                setListActive(false)
+              }}>
+              about me
+            </a>
           </li>
         </ul>
         <div
